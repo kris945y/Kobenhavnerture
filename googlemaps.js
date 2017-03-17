7a047fb1e1f49b0040a7f0b401e3f5f686aa671d
Kristian Kromand <kristiankromand@Kristians-MacBook-Pro-2.local>
2017-03-17 09:51:54 +0100

    Bootstrap nice nice

--- googlemaps.js
+++ googlemaps.js
@@ -57,6 +57,42 @@ function initMap() {
     console.log("load JSON");
     $.getJSON("googlemaps.json", hentData);
 
+
+
+
+
+    $(document).ready(function () {
+        var panels = $('.user-infos');
+        var panelsButton = $('.dropdown-user');
+        panels.hide();
+
+        //Click dropdown
+        panelsButton.click(function () {
+            //get data-for attribute
+            var dataFor = $(this).attr('data-for');
+            var idFor = $(dataFor);
+
+            //current button
+            var currentButton = $(this);
+            idFor.slideToggle(400, function () {
+                //Completed slidetoggle
+                if (idFor.is(':visible')) {
+                    currentButton.html('<i class="icon-chevron-up text-muted"></i>');
+                } else {
+                    currentButton.html('<i class="icon-chevron-down text-muted"></i>');
+                }
+            })
+        });
+
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $('button').click(function (e) {
+            e.preventDefault();
+            alert("This is a demo.\n :-)");
+        });
+    });
+
     // ****** OVERLAY HER *****
     //    var bounds = {
     //        north: 55.706752411438366,
